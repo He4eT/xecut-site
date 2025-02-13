66594590c8fe27d0874112f6d3cec6d61f71af86
He4eT <He4eT@oddsquat.org>
2025-02-13 14:53:00 +0100

    11ty: add config

---
+++ eleventy.config.mjs
@@ -0,0 +1,12 @@
+import { EleventyI18nPlugin } from '@11ty/eleventy'
+
+/**
+ * @param {import('@11ty/eleventy').UserConfig} eleventyConfig
+ */
+export default function(eleventyConfig) {
+  eleventyConfig.setInputDirectory('src')
+  eleventyConfig.setOutputDirectory('build')
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: 'en'
+  })
+}
