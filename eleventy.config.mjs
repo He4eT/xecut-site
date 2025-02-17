b7c6af41780a0f2fd300d10e0b357552a1c1b945
He4eT <He4eT@oddsquat.org>
2025-02-18 00:14:53 +0100

    css: scaffold styles

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -4,8 +4,13 @@ import { EleventyI18nPlugin } from '@11ty/eleventy'
  * @param {import('@11ty/eleventy').UserConfig} eleventyConfig
  */
 export default function(eleventyConfig) {
+  /* Source and Target*/
   eleventyConfig.setInputDirectory('src')
   eleventyConfig.setOutputDirectory('build')
+  /* Assets */
+  eleventyConfig.addPassthroughCopy('src/_assets/')
+  eleventyConfig.ignores.add('src/_assets/**');
+  /* Plugins*/
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: 'en',
   })
