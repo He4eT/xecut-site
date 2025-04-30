c9592000954e78098b4699b9bcbd2ae3a74c644f
He4eT <He4eT@oddsquat.org>
2025-04-30 23:18:45 +0200

    filters: sumObjectValues

---
+++ src/_filters/sumObjectValues.js
@@ -0,0 +1,10 @@
+/**
+ * Transform {a: 1, b: 2, c: 'skip'} to 3
+ * @param {Record<string, unknown>} obj
+ * @returns {number}
+ */
+export const sumObjectValues = (obj) => {
+  return Object.values(obj)
+    .filter((x) => typeof x === 'number')
+    .reduce((sum, x) => sum + x, 0)
+}
