207afb9bafdb1f542dde19e98a4ceb3e130b6388
He4eT <He4eT@oddsquat.org>
2026-01-29 01:45:46 +0100

    11ty: fixup typos

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -8,16 +8,16 @@ import { sumObjectValues } from './src/_filters/sumObjectValues.js'
  * @param {import('@11ty/eleventy').UserConfig} eleventyConfig
  */
 export default function(eleventyConfig) {
-  /* Source and Target*/
+  /* Source and Target */
   eleventyConfig.setInputDirectory('src')
   eleventyConfig.setOutputDirectory('build')
   /* Assets */
   eleventyConfig.addPassthroughCopy('src/_assets/css/**/*.css')
   eleventyConfig.addPassthroughCopy('src/_assets/fonts/')
   eleventyConfig.addPassthroughCopy('src/_assets/img/')
-  /* Favicon*/
+  /* Favicon */
   eleventyConfig.addPassthroughCopy({ 'src/_assets/favicon' : '/' })
-  /* Plugins*/
+  /* Plugins */
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: 'ru',
   })
